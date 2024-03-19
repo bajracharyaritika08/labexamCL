e2b810401758e7d248cc28b5f16499092aa803e8
ritika.bajracharya <ritika.bajracharya@amniltech.com>
2024-03-19 08:15:43 +0545

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h2>Hello World</h2>
+        <h1>I am Ritika Bajracharya</h1>
+        <p>This is Lab Exam of Cloud Computing.</p>
+        <i>March 19, 2024 A.D.</i>
+      </div>
+    </>
+  );
+}
+
+export default App;
